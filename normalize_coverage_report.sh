46eac23143b64b9ab44fda9f82147803399a8d9d
Ardit Marku <markoupetr@gmail.com>
2024-02-01 17:04:08 +0200

    Updates for flow-cli release v1.12.0-cadence-v1.0.0-M4-2

--- normalize_coverage_report.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/A.0000000000000007.ApprovalVoting/contracts\/ApprovalVoting.cdc/' coverage.lcov
-sed -i 's/A.0000000000000007.ArrayUtils/contracts\/ArrayUtils.cdc/' coverage.lcov
-sed -i 's/A.0000000000000007.StringUtils/contracts\/StringUtils.cdc/' coverage.lcov
-sed -i 's/A.0000000000000007.FooContract/contracts\/FooContract.cdc/' coverage.lcov
